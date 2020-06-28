f256b9ada7abf6c6326302fc82f2b23e710e8e3f
Adnan <adnan_ghafoor94@hotmail.com>
2020-06-28 20:42:13 +0100

    Set up the server on the server js file

---
+++ src/server/server.js
@@ -0,0 +1,26 @@
+// Requires
+
+const express = require("express");
+const expressHandlebar = require("express-handlebars");
+const controller = require("../controllers/burgersController");
+
+// Setting up the server
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+
+app.use("/public", express.static("../public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json);
+app.use(controller);
+
+// Setting up handlebar engine and setting it to the main as default
+
+app.engine("handlebars", expressHandlebar({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: http://localhost:${PORT}`);
+});
