5e6d39a409129cf6a3e488957cd766988c7ec361
Adnan <adnan_ghafoor94@hotmail.com>
2020-07-03 16:02:53 +0100

    Added URI?

--- src/config/connection.js
+++ src/config/connection.js
@@ -6,6 +6,8 @@ const options = {
   user: "suaaogossppkye",
   password: "d2a9049500574468ee14172221c05195b77decec57b7ae24b59d8a3f5a967e25",
   database: "d6gdpvldhaarkf",
+  uri:
+    "postgres://suaaogossppkye:d2a9049500574468ee14172221c05195b77decec57b7ae24b59d8a3f5a967e25@ec2-34-225-162-157.compute-1.amazonaws.com:5432/d6gdpvldhaarkf",
 };
 
 const connection = mysql.createConnection(options);
