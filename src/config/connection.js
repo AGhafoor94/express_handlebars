0b2bbb656c4c0a283b6e9a03b3553100f95bab21
Adnan <adnan_ghafoor94@hotmail.com>
2020-07-03 16:52:22 +0100

    JawDB options to test

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,25 +1,14 @@
 const mysql = require("mysql");
 let connection;
 
-if (process.env.JAWDB_URL) {
-  const jawDBOptions = {
-    host: "lyl3nln24eqcxxot.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    port: 3306,
-    user: "l4u7z2v6lz1xk0e9",
-    password: "nubl93gtm43oclyk",
-    database: "otuqpo7lkqcvm8p4",
-  };
-  connection = mysql.createConnection(jawDBOptions);
-} else {
-  const options = {
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db",
-  };
-  connection = mysql.createConnection(options);
-}
+const jawDBOptions = {
+  host: "lyl3nln24eqcxxot.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  port: 3306,
+  user: "l4u7z2v6lz1xk0e9",
+  password: "nubl93gtm43oclyk",
+  database: "otuqpo7lkqcvm8p4",
+};
+connection = mysql.createConnection(jawDBOptions);
 
 const onConnect = (err) => {
   if (err) throw err;
